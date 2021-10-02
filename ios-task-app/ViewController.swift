09c3bb74abbf4111846f1811d2fcddac6533abce
Son Hong Do <sondh0127@gmail.com>
2021-10-02 08:37:01 +0700

    Initial Commit

---
+++ ios-task-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-task-app
+//
+//  Created by Son Hong Do on 02/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
